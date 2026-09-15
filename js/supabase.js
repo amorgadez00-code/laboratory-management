2bc35d17301880a8ec7d76f80388f02d9b412167
April Morgadez <amorgadez00@adssu.edu.ph>
2026-09-15 16:01:25 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://bfapfbhipemiouugyahi.supabase.co";
 
-const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmYXBmYmhpcGVtaW91dWd5YWhpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4OTQ0NzcyNCwiZXhwIjoyMTA1MDIzNzI0fQ.yXIq4RZJHEmCWuT3KIkIT6FMPgpS0wGXGWfzBLjo_pw";
 
 const supabaseClient =
     window.supabase.createClient(
